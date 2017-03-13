143ee3f54f765706ba79782628c044da76a44a7f
李扬 <ly18842607263@gmail.com>
2017-03-13 21:46:34 +0800

    add author

--- DOUBAN_API/douban_API.py
+++ DOUBAN_API/douban_API.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 import urllib
 import urllib2
-
+import demjson
 doubanAPI = "https://api.douban.com/v2/book/search"
-q = raw_input('请输入查询书名')
+#q = raw_input('请输入查询书名')
+q = "三生三世十里桃花"
 count = 1
 url = '%s?q=%s&count=%d'%(doubanAPI,q,count)
-print url
 req = urllib2.Request(url)
-print req
-
 res_data = urllib2.urlopen(req)
 res = res_data.read()
-print res
+dic_json = demjson.decode(res)
+dicbook = dic_json["books"]
+author= str(dicbook[0]["author"])
+pubdate =   str(dicbook[0]["pubdate"])
+name = q
+
+
+
+
