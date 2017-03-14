234df3bf9f3b547bf0bcead894263a9283bc4bbd
李扬 <ly18842607263@gmail.com>
2017-03-14 11:01:38 +0800

    a little change

--- DOUBAN_API/douban_API.py
+++ DOUBAN_API/douban_API.py
@@ -3,7 +3,7 @@ import urllib2
 import json
 doubanAPI = "https://api.douban.com/v2/book/search"
 #q = raw_input('请输入查询书名')
-q = "三生三世十里桃花"
+q = "用python写网络爬虫"
 count = 1
 url = '%s?q=%s&count=%d'%(doubanAPI,q,count)
 req = urllib2.Request(url)
@@ -11,9 +11,10 @@ res_data = urllib2.urlopen(req)
 res = res_data.read()
 dic_json = json.loads(res)
 dicbook = dic_json["books"]
-author= str(dicbook[0]["author"])
 pubdate =   str(dicbook[0]["pubdate"])
 name = q
 for type in dic_json['books'][0]['tags'] :
-      pass
+    pass
 type = type['name']
+for author in dic_json["books"][0]['author']:
+    pass
