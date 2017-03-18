6e064057c7f30722a78a77f93bff14a7325694a3
李扬 <ly18842607263@gmail.com>
2017-03-18 22:03:31 +0800

    latest

--- Mysite/Mysite/settings.py
+++ Mysite/Mysite/settings.py
@@ -39,14 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'account',
     'upload',
-    'bookinfo'
+    'bookinfo',
+    'bootstrap_toolkit',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-#    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
