b09d9c84b0be7f373fe9b7fb2e88357e3a1ed8ac
李扬 <ly18842607263@gmail.com>
2017-03-31 10:26:34 +0800

    latest

--- Mysite/bookinfo/models.py
+++ Mysite/bookinfo/models.py
@@ -10,7 +10,7 @@ class bookinfo(models.Model):
     type = models.CharField(max_length=20)
     ISBN = models.CharField(max_length=20)
     path = models.CharField(max_length=100,default="/")
-class gitbok(models.Model):
+class gitbook(models.Model):
     url = models.CharField(max_length=50,null=False)
     author = models.CharField(max_length=20)
     path = models.CharField(max_length=100,default="/")
